b88712b7532477f04f2b4955a6b521e62cc9a865
phonchai01 <phonchaih65@nu.ac.th>
2024-09-01 20:23:31 +0700

    Update Currency.dart

--- lib/Currency.dart
+++ lib/Currency.dart
@@ -1,14 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:simple_app/screen/lastestRate.dart';
-
-void main() {
-  runApp(MaterialApp(
-    title: "Exchange Currency",
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Exchange Currency"),
-      ),
-      body: const LatestRate(),
-    ),
-  ));
+import 'dart:convert';
+Currency currencyFromJson(String str) => Currency.fromJson(json.decode(str));
+String currencyToJson(Currency data) => json.encode(data.toJson());
+class Currency {
+    bool? success;
+    Map<String, String>? result;
+    Currency({
+        this.success,
+        this.result,
+    });
+    factory Currency.fromJson(Map<String, dynamic> json) => Currency(
+        success: json["success"],
+        result: Map.from(json["result"]!).map((k, v) => MapEntry<String, String>(k, v)),
+    );
+    Map<String, dynamic> toJson() => {
+        "success": success,
+        "result": Map.from(result!).map((k, v) => MapEntry<String, dynamic>(k, v)),
+    };
 }
