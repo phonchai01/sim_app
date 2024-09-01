fac4821ece8320c7544c4640522023cc9887984f
phonchai01 <phonchaih65@nu.ac.th>
2024-09-01 20:24:20 +0700

    rate.dart

--- lib/rate.dart
+++ lib/rate.dart
@@ -1,14 +1,37 @@
-import 'package:flutter/material.dart';
-import 'package:simple_app/screen/lastestRate.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(MaterialApp(
-    title: "Exchange Currency",
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Exchange Currency"),
-      ),
-      body: const LatestRate(),
-    ),
-  ));
+Rate rateFromJson(String str) => Rate.fromJson(json.decode(str));
+
+String rateToJson(Rate data) => json.encode(data.toJson());
+
+class Rate {
+    bool? success;
+    int? timestamp;
+    DateTime? date;
+    String? base;
+    Map<String, double>? result;
+
+    Rate({
+        this.success,
+        this.timestamp,
+        this.date,
+        this.base,
+        this.result,
+    });
+
+    factory Rate.fromJson(Map<String, dynamic> json) => Rate(
+        success: json["success"],
+        timestamp: json["timestamp"],
+        date: json["date"] == null ? null : DateTime.parse(json["date"]),
+        base: json["base"],
+        result: Map.from(json["result"]!).map((k, v) => MapEntry<String, double>(k, v?.toDouble())),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "success": success,
+        "timestamp": timestamp,
+        "date": "${date!.year.toString().padLeft(4, '0')}-${date!.month.toString().padLeft(2, '0')}-${date!.day.toString().padLeft(2, '0')}",
+        "base": base,
+        "result": Map.from(result!).map((k, v) => MapEntry<String, dynamic>(k, v)),
+    };
 }
