eb21ed4e59732b4d4c17b55eb9d897f3f6a337a2
phonchai01 <phonchaih65@nu.ac.th>
2024-09-01 20:26:02 +0700

    Add files via upload

---
+++ lib/scene/Convert.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:simple_app/screen/lastestRate.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Exchange Currency",
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Exchange Currency"),
+      ),
+      body: const LatestRate(),
+    ),
+  ));
+}
